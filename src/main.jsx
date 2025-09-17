b69bb12931bc48fe301b1b4c097de63e98a795a1
deepanshu-QM <cdeepanshu231@gmail.com>
2025-09-18 03:58:49 +0530

    Props Children

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,38 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-class Header extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {favoritecolor : "red"};
-    }
-
-    componentDidMount(){
-        setTimeout(() => {
-            this.setState({favoritecolor:"green"});
-        }, 6000)
-    }
-    render(){
-        return(
-            <h1>This Color is Changing to {this.state.favoritecolor}</h1>
-        );
-    }
+function Son(props){
+    return(
+        <div style={{backgroundColor:"lightpink"}}>
+            <h2>This is Your Son David  Lives near {props.location}</h2>
+            <div>{props.FromParent}</div>
+        </div>
+    );
 }
 
-createRoot(document.getElementById('root')).render(<Header/>)
+function Daughter(props){
+    const {location, shopping, Vlogs} = props;
+    return(
+        <div style={{backgroundColor:"lightpink"}}>
+            <h2>Liyana is in {props.location} because she loves {props.shopping} , {props.Vlogs}</h2>
+            <div>{props.FromParent}</div>
+        </div>
+    );
+}
+
+function Parents(){
+    return(
+        <div>
+            <h1>Son and daughter a Beautifull Gift I have </h1>
+            <Son location="New-York">
+                <p>This is Written in parent componenet and will Displayed in son component</p>
+            </Son>
+
+            <Daughter location="Infinity - mall" shopping = "Clothing " Vlogs="Vlogging">
+                <p>This is Written in parent componenet and will Displayed in Daughter component</p>
+            </Daughter>
+        </div>
+    );
+}
+
+createRoot(document.getElementById('root')).render(<Parents/>)
