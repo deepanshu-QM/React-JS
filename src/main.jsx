29868b37635d23c39cfee60e7e6f5730ca5b19f1
deepanshu-QM <cdeepanshu231@gmail.com>
2025-09-20 02:00:50 +0530

    authetication

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,61 @@
-//Controlled cOMPONENT IN rEACT 
 
+//Controlled Component (Repetitive code )
 
-import React, {useState} from "react";
+import React ,{useState} from "react";
 import { createRoot } from "react-dom/client";
-function NameForm(){
-    //Declare a state to store the values 
-    const [name , setName] = useState("")
 
-    //Update the state when user type Something 
-    function HandleChanges(event){
-        setName(event.target.value);                //etv
+function Authentication(){
+    //Declare seprate state to store Values 
+    const [name , setName] = useState("");
+    const [password, setPassword] = useState("");
+    const [emailId, setemailId] = useState("");
+
+    //Updates the changes when user Type Somethings
+    function HandleUpdates(event){
+        const {name, value} = event.target;
+
+        if(name == "username"){
+            setName(value);
+        }
+        else if(name == "password"){
+            setPassword(value);
+        }else if(name == "emailId"){
+            setemailId(value);
+        }
     }
+
     return(
         <div>
-            <input type="Text" value= {name} onChange={HandleChanges}/>
-            <p>Your Name is : {name}</p>
+            <input 
+            type="Text"
+            name = "username"
+            placeholder="Enter Name "
+            value={name}
+            onChange={HandleUpdates}/>
+            <br/>
+            <br/>
+            <br/>
+            <input 
+            type="password"
+            name="password"
+            placeholder="Enter password"
+            value={password}
+            onChange={HandleUpdates}/>
+            <br/>
+            <br/>
+            <br/>
+            <input 
+            type="emailId"
+            name="emailId"
+            placeholder="Enter-EmailID"
+            value={emailId}
+            onChange={HandleUpdates}/>
+
+            <p>Name :{name}</p>
+            <p>password : {password}</p>
+            <p>Email-Id : {emailId}</p>
         </div>
     )
 }
 
-createRoot(document.getElementById('root')).render(<NameForm/>)
+createRoot(document.getElementById('root')).render(<Authentication/>)
