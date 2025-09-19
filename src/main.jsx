7dec9c3111ff8771d142ac95095561c274ac8eaf
deepanshu-QM <cdeepanshu231@gmail.com>
2025-09-19 13:41:17 +0530

    condition &&

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,13 @@
 
-// Render React component using conditions 
+// Render React component using conditions &&
 
 import { createRoot } from "react-dom/client";
 
-function MissedGoal(){
+function CheckAnswer(props){
     return(
-        alert("You Missed Goal")
+        <>
+        {props.AttempSideQuestions && <h1>I Got {props.AttempSideQuestions} news of Your result </h1>}
+        </>
     )
 }
-
-function MadeGoal(){
-    return(
-        alert("Nailed it .. , A Goal")
-    )
-}
-
-function CheckGoal(props){
-    const isGoal = props.isGoal;
-    if(isGoal){
-        return <MissedGoal/>;
-    }
-    return <MadeGoal/>;
-
-}
-
-createRoot(document.getElementById('root')).render(<CheckGoal isGoal = {true}/>)
+createRoot(document.getElementById('root')).render(<CheckAnswer AttempSideQuestions = "Good"/>)
