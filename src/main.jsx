b727fd00b27c0fb33d72b0c298031aeb1d611d66
deepanshu-QM <cdeepanshu231@gmail.com>
2025-09-19 12:40:57 +0530

    React Event <Button>

--- src/main.jsx
+++ src/main.jsx
@@ -1,38 +1,17 @@
+//React Events 
+
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-function Son(props){
+function TakeAshot(){
+    const shoot = () => {
+        alert("Great Shot ! ..")
+    }
+
     return(
-        <div style={{backgroundColor:"lightpink"}}>
-            <h2>This is Your Son David  Lives near {props.location}</h2>
-            <div>{props.FromParent}</div>
-        </div>
-    );
+        <button onClick= {shoot}>Take_Your_Shot</button>
+    )
 }
 
-function Daughter(props){
-    const {location, shopping, Vlogs} = props;
-    return(
-        <div style={{backgroundColor:"lightpink"}}>
-            <h2>Liyana is in {props.location} because she loves {props.shopping} , {props.Vlogs}</h2>
-            <div>{props.FromParent}</div>
-        </div>
-    );
-}
+createRoot(document.getElementById('root')).render(<TakeAshot/>)
 
-function Parents(){
-    return(
-        <div>
-            <h1>Son and daughter a Beautifull Gift I have </h1>
-            <Son location="New-York">
-                <p>This is Written in parent componenet and will Displayed in son component</p>
-            </Son>
-
-            <Daughter location="Infinity - mall" shopping = "Clothing " Vlogs="Vlogging">
-                <p>This is Written in parent componenet and will Displayed in Daughter component</p>
-            </Daughter>
-        </div>
-    );
-}
-
-createRoot(document.getElementById('root')).render(<Parents/>)
