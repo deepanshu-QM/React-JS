8483b1969f84681657a0f727a1fbde4f398d4d47
deepanshu-QM <cdeepanshu231@gmail.com>
2025-09-19 14:01:10 +0530

    List map()

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,26 @@
 
-// Render React component using conditions &&
+//Rendering List through map() array 
+
 
 import { createRoot } from "react-dom/client";
 
-function CheckAnswer(props){
+function Fruits(){
+    const fruits = ["Apple", "Mango", "Gauva", "WaterMelon"];
     return(
         <>
-        {props.AttempSideQuestions && <h1>I Got {props.AttempSideQuestions} news of Your result </h1>}
+        <h1>Here is list Of fruits Liked my his-Highness</h1>
+        <ul>
+            {fruits.map((fruits) => <li>I Like { fruits }</li>)}
+        </ul>
         </>
     )
 }
-createRoot(document.getElementById('root')).render(<CheckAnswer AttempSideQuestions = "Good"/>)
+
+createRoot(document.getElementById('root')).render(<Fruits/>)
+
+/*    Alternative 
+fruits.map(function(fruits) {
+<li>I like {fruits}</li>
+}
+
+*/
